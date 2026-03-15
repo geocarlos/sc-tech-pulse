b2141923b5ac15f325f02f4b8c74a0dab2162137
Geocarlos <geowildcat@gmail.com>
2026-03-14 23:27:54 -0300

    Add dbt transformations step

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+import subprocess
 import logging
 from src.generator import generate_sc_data as generate_mock_data
 from src.ingestion import ingest_data as load_to_duckdb
@@ -9,10 +11,14 @@ def run_pipeline():
     logging.info("Step 2: Ingesting into DuckDB...")
     load_to_duckdb()
 
-    logging.info("Step 3: To be implemented...")
-    # TO-DO: Trigger dbt core directly from Python
+    logging.info("Step 3: Running dbt Transformations...")
    
-    logging.info("Pipeline completed successfully!")
+    result = subprocess.run(["dbt", "build"], capture_output=True, text=True)
+    
+    if result.returncode == 0:
+        logging.info("Pipeline completed successfully!")
+    else:
+        logging.error(f"dbt failed: {result.stderr}")
 
 if __name__ == "__main__":
     run_pipeline()
